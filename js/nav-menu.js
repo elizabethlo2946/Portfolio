4efcb0ac93c49f531761030d9dde14288d6f6a5a
eylo <elizabethlo2946@gmail.com>
2018-03-13 14:00:53 -0700

    Beginning NAV collapse js
    Setting up the variables for using javascript to collapse the nav bar for mobile and desktop view in a responsive manner

---
+++ js/nav-menu.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("JavaScript running");
+
+var width = window.innerWidth;
+console.log("Current window width: "+width+"px");
+
+var nav-toggle = document.querySelector("#nav-toggle");
+var nav-items = document.querySelector("#nav-items");
+var navFirstItem = document.querySelector("#nav-items a");
