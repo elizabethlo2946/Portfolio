f252e944bd757137cedc7c7600ac735d690d4a21
eylo <elizabethlo2946@gmail.com>
2018-03-13 14:25:29 -0700

    menu BTN not working, but JS collapse is gucci my dudes
    flex that window width. Admire its collapsable beauty. Now for the button functionality...

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,9 +1,63 @@
+// following same structure as 339 Lecture 7
 "use strict";
 console.log("JavaScript running");
 
 var width = window.innerWidth;
 console.log("Current window width: "+width+"px");
 
-var nav-toggle = document.querySelector("#nav-toggle");
-var nav-items = document.querySelector("#nav-items");
+var navToggle = document.querySelector("#nav-toggle");
+var navItems = document.querySelector("#nav-items");
 var navFirstItem = document.querySelector("#nav-items a");
+
+window.onresize = resetNavBarMobile;
+window.onload = resetNavBarMobile;
+
+navToggle.addEventListener("click",
+   function() {
+      console.log("menu button clicked");
+
+      if(navItems.classList.contains("hidden")) {
+         console.log("showing #nav-items");
+         // remove css hidden attribute on these items
+         navItems.classList.remove("hidden");
+         navItems.setAttribute("aria-hidden", "false");
+
+         // navToggle is visible
+         navToggle.setAttribute("aria-expanded", "true");
+
+         navFirstItem.focus();
+      } else {
+         console.log("hiding nav items");
+         // bring back css hidden attribute on these navItems
+         navItems.classList.add("hidden");
+         navItems.setAttribute("aria-hidden", "true");
+
+         // navToggle is not visible
+         navToggle.setAttribute("aria-expanded", "false");
+      }
+   }
+);
+
+function resetNavBarMobile() {
+   if (width < 675) {
+      console.log("Collapsing Menu @ breakpoint");
+
+      // make the menu button visible
+      navToggle.classList.remove("hidden");
+      navToggle.setAttribute("aria-hidden", "false");
+
+      // hide the nav nav
+      navItems.classList.add("hidden");
+      navItems.setAttribute("aria-hidden", "true");
+      // make navItems controlled by the button
+      navItems.setAttribute("aria-labelledby", "nav-toggle");
+   } else {
+      // keep the menu button hidden
+      navToggle.classList.add("hidden");
+      navToggle.setAttribute("aria-hidden", "true");
+
+      // show the nav items
+      navItems.classList.remove("hidden");
+      navItems.setAttribute("aria=hidden", "false");
+   }
+}
